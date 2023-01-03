8718ca90d6e10b97a633c00ce1a22e4d508d1868
Reander Agulto <reanderagulto29@gmail.com>
2023-01-03 16:12:12 +0800

    Updated code - Added blade layouts and Images. Added database seed and factory as well as table

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Json API respnse /api/<name>
+// Route::get('/posts', function(){
+//     return response()->json([
+//         'posts' => [
+//             [
+//                 'title' => 'Post One'
+//             ]
+//         ]
+//     ]);
+// });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All Listings
 Route::get('/', function () {
-    return view('welcome');
+    return view(
+        'listings', 
+        [
+            'heading' => 'Latest Listings',
+            'listings' => Listing::all(),
+        ]
+    );
 });
+
+Route::get('/listing/{listing}', function(Listing $listing){
+    return view('listing', [
+        'listing' => $listing,
+    ]);
+});
+
+// // Custom Headers
+// Route::get('/hello', function() {
+//     return response("<h1>Hello World</h1>", 200)
+//         ->header('Content-Type', 'text/plain');
+// });
+
+// // wildcards and regex constraints
+// Route::get('/posts/{id}', function($id){
+//     dd($id);
+//     return response('Post: ' . $id);
+// })->where('id', '[0-9]+');
+
+// // Query Parameters
+// Route::get('/search', function(Request $request){    
+//     return $request->name . ' ' . $request->city;
+// });
